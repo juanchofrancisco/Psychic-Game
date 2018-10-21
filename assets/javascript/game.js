354662d8b4bd96f5881e61c7d319c3a282c5090e
Juan F. Hernandez <juanhernandez@Juans-MacBook-Air.local>
2018-10-20 22:04:09 -0500

    V1.0, game is working for the most part, next version will have bootstrap styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,72 @@
+// letter choices available to the computer and user
+var computerChoices = "abcdefghijklmnopqrstuvwxyz";
+
+
+// all variables for game, are these considered global variables??
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var userLettersGuessed = []; 
+var computerGuess = [];
+
+// computer selects random letter, new variable of compGuess is introduced which = random computerchoices (alphabet)
+// by using the math.floor , computerguess value is added by .push(compguess)
+
+window.onload = function() {
+	var compGuess = computerChoices[Math.floor(Math.random() * 26)];
+	computerGuess.push(compGuess);
+	
+}
+
+
+// actual game
+// on key up event, new local variable is introduced, playerGuess, which equals the pressed key, user letter guessed
+//is then pushed into the player guess array
+document.onkeyup = function(event) {
+	var playerGuess = event.key;
+	userLettersGuessed.push(playerGuess);
+    
+    
+// beginning of if statements logic scenarios
+// if player guess = computer guess, and guesses are greater than zero, then alert you win! add to wins
+// I then reiterated the variables to start the game, I couldn't figure out a separate reset function
+
+
+if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) { 
+    wins++;
+    alert("You Win!");
+	guessesLeft = 9;
+	userLettersGuessed.length = 0;
+	computerGuess.length = 0;
+	var compGuess = computerChoices[Math.floor(Math.random() * 26)];
+	computerGuess.push(compGuess);
+	
+}
+
+// else, if player guess is not equal to the computer guess & guesses are greater than zero, then decrement from
+// guesses left
+else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
+	guessesLeft = guessesLeft-1;
+}
+// alert you loose, add to losses and start over, restore variable values
+
+else {
+    losses++;
+    alert("You Loose!");
+	guessesLeft = 9;
+	userLettersGuessed.length = 0;
+	computerGuess.length = 0;
+	var compGuess = computerChoices[Math.floor(Math.random() * 26)];
+	computerGuess.push(compGuess);
+	
+}
+// update html with 
+var html = "<h1>The Psychic Game</h1>" + "<p>Guess what letter I'm thinking of!</p>" +
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Losses: " + losses + "</p>" +
+          "<p>Guesses left: " + guessesLeft + "</p>" +
+          "<p>Your guesses so far: " + userLettersGuessed + "</p>";
+ 
+document.querySelector("#game").innerHTML = html;
+
+}
